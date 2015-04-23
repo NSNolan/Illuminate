761d3fa3b3495703a7892b48429dc613f00487de
Nolan Astrein <nastrein@apple.com>
2015-04-23 00:57:44 -0700

    Initial Draft

---
+++ Illuminate/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Illuminate
+//
+//  Created by Nolan Astrein on 4/22/15.
+//  Copyright (c) 2015 Nolan Astrein. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ Illuminate/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Illuminate
+//
+//  Created by Nolan Astrein on 4/22/15.
+//  Copyright (c) 2015 Nolan Astrein. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
